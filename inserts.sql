61d224a88e1b58a0822db1a7dda4ad4f49aaf295
Esteban Durán <eiduran@estudiantec.cr>
2021-06-25 00:48:56 -0600

    Inserts script added

---
+++ inserts.sql
@@ -0,0 +1,13 @@
+Insert into Severity(`name`) Values ('Info')
+Insert into Severity(`name`) Values ('Error')
+Insert into Severity(`name`) Values ('Warning')
+
+Insert into EntityTypes(`name`) Values ('User')
+Insert into EntityTypes(`name`) Values ('Invoice')
+
+Insert into AppSources(`name`) Values ('App')
+Insert into AppSources(`name`) Values ('Server')
+
+Insert into LogTypes(`name`) Values ('Match completed')
+Insert into LogTypes(`name`) Values ('Matchmaking started')
+Insert into LogTypes(`name`) Values ('Matchtracing analysis')
